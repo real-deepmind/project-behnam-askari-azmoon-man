7ed4dab54c3aec67acf08cc918e3ecb240b784bd
Behnam <realdeepmind@gmail.com>
2025-12-15 09:09:33 +0330

    Initial commit Add initial package structure

---
+++ src/main/java/realdeepmind/ProjectBehnamAskariAzmoonManApplication.java
@@ -0,0 +1,13 @@
+package realdeepmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBehnamAskariAzmoonManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBehnamAskariAzmoonManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/realdeepmind/ProjectBehnamAskariAzmoonManApplicationTests.java
@@ -0,0 +1,13 @@
+package realdeepmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBehnamAskariAzmoonManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
