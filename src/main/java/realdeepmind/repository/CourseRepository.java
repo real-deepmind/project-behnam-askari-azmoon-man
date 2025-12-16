8a4a54ff1d860bdf45a505789cf8b73821ad57cc
Behnam <realdeepmind@gmail.com>
2025-12-16 19:30:23 +0330

    refactor : change CourseRepository.java because Changes of entity structure.

--- src/main/java/realdeepmind/repository/CourseRepository.java
+++ src/main/java/realdeepmind/repository/CourseRepository.java
@@ -1,23 +1,31 @@
 package realdeepmind.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import realdeepmind.entity.Course;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Optional<Course> findByCourseCode(String courseCode);
 
     boolean existsByCourseCode(String courseCode);
 
-    List<Course> findByTeacher_Id(Long teacherId);
+    @Query("SELECT c FROM Course c JOIN c.enrollments e WHERE e.user.id = :teacherId AND e.roleInCourse = 'TEACHER'")
+    List<Course> findCoursesByTeacherId(@Param("teacherId") Long teacherId);
 
-    List<Course> findByStudents_Id(Long studentsId);
-
-    List<Course> findByTeacher_IdAndStudents_Id(Long teacherId, Long studentsId);
+    @Query(
+            value = "SELECT c.* FROM courses c " +
+                    "INNER JOIN course_enrollments ce ON c.id = ce.course_id " +
+                    "WHERE ce.user_id = :userId AND ce.role_in_course = 'STUDENT'",
+            nativeQuery = true
+    )
+    List<Course> findCoursesByStudentId(@Param("userId") Long userId);
 
     List<Course> findByTitleContainingIgnoreCase(String title);
 
