bc5a0631ffbf46ed59e1e7febbf271eea4c70017
Behnam <realdeepmind@gmail.com>
2025-12-18 00:15:51 +0330

    feat: create UserDetailServiceImpl and CustomUserDetails.java .

---
+++ src/main/java/realdeepmind/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package realdeepmind.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import realdeepmind.entity.User;
+import realdeepmind.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
